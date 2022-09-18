b8a3cbfeca59010459b969d807944e8b5c600a63
Andrew <56676505+Andrew-Colman@users.noreply.github.com>
2022-09-18 19:57:55 -0300

    Fix build on CI (#6)
    * Fix build on CI
    
    * update @rollup/plugin-typescript
    
    * Update typescript
    
    * Fix glob pattern (ubuntu )
    
    * Fix bundling and emitting types

--- rollup.config.js
+++ rollup.config.js
@@ -6,13 +6,21 @@ import dts from 'rollup-plugin-dts';
 import del from 'rollup-plugin-delete';
 import command from 'rollup-plugin-command';
 
+const inputDir = './src';
+const outputDir = './dist';
+const declarationDir = `${outputDir}/_declaration`;
+
 const plugins = [
+    command(
+        `yarn tsc --emitDeclarationOnly --declaration --declarationDir ${declarationDir}`,
+        { wait: true, once: true }
+    ),
     typescript({ tsconfig: './tsconfig.json' }),
     commonjs(),
     nodeResolve(),
     command('yarn minify'), // <- if you want to minify code (with terser)
     del({
-        targets: 'dist/*',
+        targets: outputDir,
     }),
 ];
 
@@ -20,12 +28,12 @@ const external = ['/node_modules/']; // external packages
 
 const emitTypes = {
     // emitting types (bundled)
-    input: './dist/.declaration/src/index.d.ts',
-    output: [{ file: './dist/index.d.ts', format: 'es' }],
+    input: `${declarationDir}/index.d.ts`,
+    output: [{ file: `${outputDir}/index.d.ts`, format: 'es' }],
     plugins: [
         dts(),
         del({
-            targets: 'dist/.declaration',
+            targets: `${declarationDir}`,
             hook: 'buildEnd',
         }),
     ],
@@ -33,9 +41,9 @@ const emitTypes = {
 
 export default [
     {
-        input: 'src/index.ts',
+        input: `${inputDir}/index.ts`,
         output: {
-            file: 'dist/index.js',
+            file: `${outputDir}/index.js`,
             format: 'commonjs',
         },
         plugins,
